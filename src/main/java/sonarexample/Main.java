51d9aa1b3595db0c2aef74ab13bf73b37310e8eb
Praveen Meruga <praveen.meruga@ideacrestsolutions.com>
2018-10-07 19:35:27 -0400

    added code smell

--- src/main/java/sonarexample/Main.java
+++ src/main/java/sonarexample/Main.java
@@ -1,7 +1,15 @@
 package sonarexample;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
     public static void main(String[] args) {
 
+        //Adding some non compliant code to introduce Code smell.
+        AtomicInteger aInt1 = new AtomicInteger(0);
+        AtomicInteger aInt2 = new AtomicInteger(0);
+
+        if (aInt1.equals(aInt2)) {
+        }
     }
 }
