914628f0bdedcc76274e3c7bee3daf0952c1da81
Praveen Meruga <praveen.meruga@ideacrestsolutions.com>
2018-10-07 19:54:47 -0400

    added more code smells

--- src/main/java/sonarexample/Main.java
+++ src/main/java/sonarexample/Main.java
@@ -2,7 +2,12 @@ package sonarexample;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Deprecated
 public class Main {
+
+    public static String CONST = "CONST";
+
+    @Deprecated
     public static void main(String[] args) {
 
         //Adding some non compliant code to introduce Code smell.
@@ -10,6 +15,13 @@ public class Main {
         AtomicInteger aInt2 = new AtomicInteger(0);
 
         if (aInt1.equals(aInt2)) {
+
         }
+
+        int target = -5;
+        int num = 3;
+
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
     }
 }
