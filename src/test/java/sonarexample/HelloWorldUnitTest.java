8014910cbee4353559b9230a7b1fff89aa9be952
Praveen Meruga <praveen.meruga@ideacrestsolutions.com>
2018-10-07 19:20:52 -0400

    skipTests Test

--- src/test/java/sonarexample/HelloWorldUnitTest.java
+++ src/test/java/sonarexample/HelloWorldUnitTest.java
@@ -1,5 +1,6 @@
 package sonarexample;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import sonarexample.HelloWorld;
 
@@ -7,6 +8,16 @@ public class HelloWorldUnitTest {
 
     @Test
     public void test() {
+        new HelloWorld().coveredByUnitTest();
+    }
+
+    @Ignore
+    public void skipTest() {
+        new HelloWorld().coveredByUnitTest();
+    }
+
+    @Ignore
+    public void skipTest2() {
         new HelloWorld().coveredByUnitTest();
     }
 
