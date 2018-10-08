9169ffb8f3366788cf258f92c6f8693e8b8d2fe5
Praveen Meruga <praveen.meruga@ideacrestsolutions.com>
2018-10-07 20:55:55 -0400

    correcting jenkinsfile

--- src/main/java/sonarexample/Main.java
+++ src/main/java/sonarexample/Main.java
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Deprecated
 public class Main {
 
-    public static String CONST = "CONST";
+/*    public static String CONST = "CONST";
 
     @Deprecated
     public static void main(String[] args) {
@@ -23,5 +23,5 @@ public class Main {
 
         target =- num;  // Noncompliant; target = -3. Is that really what's meant?
         target =+ num; // Noncompliant; target = 3
-    }
+    }*/
 }
